4f6994498b9f0d39baf955fe8e59cb2022328150
Andrzej Pisarek <andrzej.r.pisarek@gmail.com>
2017-05-28 02:52:12 +0200

    Add rmse

--- app.py
+++ app.py
@@ -6,9 +6,29 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
+def RMSE(true, pred, axis=None):
+    """
+    Function computes RMSE.
+
+    :param true: expected values
+    :param pred: predicted values
+    :return: computed RMSE
+    """
+    if hasattr(true, 'values'):
+        true = true.values
+    if hasattr(pred, 'values'):
+        pred = pred.values
+    diff = true - pred
+    mse = np.mean(diff ** 2, axis=axis)
+    rmse = np.sqrt(mse)
+    return rmse
 
 
-
+@app.route('/submission', methods=['POST'])
+def submit():
+    content = request.json
+    df = pd.DataFrame.from_dict(content)
+    return str(RMSE(df, df))
 
 
 @app.route('/', methods=['GET'])
